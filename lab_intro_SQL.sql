20d2f72f606d1cd24c8c2b91ac423be271fd9068
Marta Uzcanga <marta.uzcanga@gmail.com>
2022-05-10 18:37:14 +0200

    lab answers

---
+++ lab_intro_SQL.sql
@@ -0,0 +1,18 @@
+USE sakila;
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+SELECT * FROM actor;
+
+SELECT title
+FROM film; 
+
+SELECT name as langauge FROM language;
+
+SELECT count(store_id) FROM store;
+
+SELECT count(staff_id) FROM staff;
+
+SELECT first_name
+FROM staff; 
+
